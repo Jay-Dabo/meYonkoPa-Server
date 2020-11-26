121c885c6f96a5d6d0ded92b7e27c9fbfbb2758d
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-11-26 18:25:19 +0000

    Model and Controller function updates

--- models/professional.js
+++ models/professional.js
@@ -7,17 +7,18 @@ const cryptoRandomString = require('crypto-random-string'); // Use Cryptographic
 // Create Schema for Professionals in MongoDb
 const professionalSchema = new Schema({
     _id: { type: String, default: 'myp' + cryptoRandomString({ length: 5 }) },
+    title: { type: String, required: true, default: 'Counselor' },
     first_name: { type: String, required: true },
     other_names: { type: String },
     last_name: { type: String, required: true },
+    email: { type: String, lowercase: true, match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/], unique: true, required: true },
     gender: { type: String },
     profession: { type: Schema.Types.String, ref: 'medicalField', default: 'Counselor' },
     institute: { type: String },
-    email: { type: String, lowercase: true, match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/], unique: true, required: true },
     phone_number: { type: String, required: true },
-    is_consultant: { type: Boolean, default: false },
     password: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
     password_confirmation: { type: String, min: [8, 'Too short, min 4 characters are required'], required: true },
+    is_consultant: { type: Boolean, default: false }
 },
 { 
     timestamps: true 
