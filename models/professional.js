c3fcf39a32d3563deb6f896069c6d93c241b2315
Jeffrey Dabo <jantwiamoah@gmail.com>
2020-12-02 17:34:50 +0000

    Professional model edits

--- models/professional.js
+++ models/professional.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose'); // Require Mongooge for connection to Data
 const Schema = mongoose.Schema // Require Schema Instace of Mongoose
 const bcrypt = require('bcryptjs'); // Require BCrypt for Password encryption
 
+// Gender Enumerations
+const genders = Object.freeze({
+    Male: 'Male',
+    Female: 'Female',
+    Other: 'Non Binary',
+});
 
 // Create Schema for Professionals in MongoDb
 const professionalSchema = new Schema({
     _id: { type: String },
-    title: { type: String, required: true, default: 'Counselor' },
+    title: { type: String, default: 'Counselor' },
     first_name: { type: String, required: true },
     other_names: { type: String },
     last_name: { type: String, required: true },
     email: { type: String, lowercase: true, match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/], unique: true, required: true },
-    gender: { type: String },
+    gender: { type: String, required: true, enum: Object.values(genders) },
     profession: { type: Schema.Types.String, ref: 'medicalField', default: 'Counselor' },
     institute: { type: String, default: 'null' },
     phone_number: { type: String, required: true },
@@ -22,6 +28,8 @@ const professionalSchema = new Schema({
 { 
     timestamps: true 
 });
+
+Object.assign(professionalSchema.statics, { genders });
 
 professionalSchema.pre('save', function(next) {
     const professional = this
